be0509c7ee3657460342a2b54f43e4f5e5063b44
Minewolf01 <suchakreesurarak110@gmail.com>
2025-04-07 13:27:42 +0700

    Wow

---
+++ src/main/java/net/minewolf/firstmod/FirstMod.java
@@ -0,0 +1,16 @@
+package net.minewolf.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// Wow
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/minewolf/firstmod/FirstModClient.java
@@ -0,0 +1,10 @@
+package net.minewolf.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
